808344f955ee7b083df64af7e9a310409b24f9e9
Mahlon Reese <jmficde@jmfamily.com>
2025-01-09 18:21:47 -0500

    Removed styling for index.css, added column component. Need to add column box and story component

---
+++ omegaprojectmanagement.client/src/Column.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+//import { Droppable } from "@hello-pangea/dnd";
+import './Column.css';
+
+function Column({name}){
+
+
+    return (
+        <div>
+
+            <div>{name}</div>
+
+        </div>
+        
+
+    );
+}
+
+export default Column;
