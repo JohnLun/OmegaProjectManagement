b61aa368d93429d82aa06ea1ff1e2705669200c6
Mahlon Reese <jmficde@jmfamily.com>
2025-01-10 16:52:50 -0500

    got the fundamentals down, just need to improve on some styling on monday along with the create story button

--- omegaprojectmanagement.client/src/pages/Kanban.jsx
+++ omegaprojectmanagement.client/src/pages/Kanban.jsx
@@ -7,27 +7,32 @@ function App() {
 
     return (
         <>
-            <div className="row">
-                <div className="col">
-                    <Column name="backlog" endpoint="backlog"></Column>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <h2 class="navbar-heading p-3">Omega</h2>
+                <div class="d-flex justify-content-end w-100">
+                    <button class="btn btn-outline-success my-2 " type="submit">Create Story</button>
                 </div>
-                <div className="col">
-                    <Column name="In Progress" endpoint="in-progress"></Column>
-                </div>
-                <div className="col">
-                    <Column name="Ready For Testing" endpoint="in-testing"></Column>
-                </div>
-                <div className="col">
-                    <Column name="Completed" endpoint="in-complete"></Column>
+                
+            </nav>
+
+            <div className="container-fluid mt-4" style={{ height: "calc(100vh - 56px)" }}>
+                <div className="d-flex justify-content-between h-100" style={{ gap: "1rem" }}>
+                    <div className="bg-primary text-black p-4" style={{ flex: "1", maxWidth: "24%" }}>
+                        <Column name="Backlog" endpoint="backlog"></Column>
+                    </div>
+                    <div className="bg-primary text-black p-4" style={{ flex: "1", maxWidth: "24%" }}>
+                        <Column name="In Progress" endpoint="in-progress"></Column>
+                    </div>
+                    <div className="bg-primary text-black p-4" style={{ flex: "1", maxWidth: "24%" }}>
+                        <Column name="Ready For Testing" endpoint="in-testing"></Column>
+                    </div>
+                    <div className="bg-primary text-black p-4" style={{ flex: "1", maxWidth: "24%" }}>
+                        <Column name="Completed" endpoint="in-complete"></Column>
+                    </div>
                 </div>
             </div>
-           
-            
-            
-            
-        </>
 
-        
+        </>
     );
 }
 
